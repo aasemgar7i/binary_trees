b234bbb9b734de0d9b88083d1f307451c1fc3cab
aasemgar7i <aasemgar7i@gamil.com>
2024-02-27 21:16:00 +0200

    task 1 fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
+
 	/* If the parent already has a left child */
 	if (parent->left != NULL)
 	{
