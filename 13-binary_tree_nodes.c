7f65dd1631f6e59a9cf91c41ce4382b8f58a4335
aasemgar7i <aasemgar7i@gamil.com>
2024-02-29 21:36:19 +0200

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,28 +5,11 @@
  *@tree: The tree to be evaluated
  *Return: 0 when tree is NULL or a recursive function call
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	/* Base case: if tree is NULL, return 0 */
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	/* Initialize count to 0 */
-	size_t count = 0;
-
-	/* If the current node has at least one child, increment count */
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		count++;
-
-	/* Recursively count nodes with at least one child in the left subtree */
-	count += binary_tree_nodes(tree->left);
-
-	/* Recursively count nodes with at least one child in the right subtree */
-	count += binary_tree_nodes(tree->right);
-	}
-	/* Return the total count */
-	return (count);
+	if (tree != NULL && (tree->left != NULL || tree->right != NULL))
+		return (1 + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
+	return (0);
 }
